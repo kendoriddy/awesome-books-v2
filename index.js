7cff6088f1a1c57c81461f9631f75624ac65fc9f
Kehinde Onifade <onifkay@gmail.com>
2022-08-08 22:16:23 +0100

    Added date-time function with luxon

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { DateTime } from './modules/luxon.js';
 import { showBooks } from './modules/show.js';
 import { handlePageChange } from './modules/page.js';
 
@@ -14,3 +15,31 @@ nav.forEach((el) => {
     handlePageChange(e);
   });
 });
+
+class DateTimeLuxon {
+  constructor() {
+    this.dt = '';
+    this.options = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    };
+  }
+
+  getTime() {
+    this.dt += DateTime.now().setLocale('en-US')
+      .toLocaleString(this.options)
+      .split(',')
+      .splice(1);
+    this.dt += ', ';
+    this.dt += DateTime.now().setLocale('en-US').toLocaleString(DateTime.TIME_WITH_SECONDS);
+    return this.dt;
+  }
+}
+
+setInterval(() => {
+  const getClock = document.querySelector('.date');
+  const timeStr = new DateTimeLuxon().getTime();
+  getClock.textContent = timeStr;
+}, 1000);
